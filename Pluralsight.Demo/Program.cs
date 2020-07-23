b00644312f42a36020825eeb87b3ea8f70b214ed
Marcos Robles <ecko_lob@outlook.com>
2020-07-23 11:17:11 -0500

    Assigning a claim

--- Pluralsight.Demo/Program.cs
+++ Pluralsight.Demo/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,12 +13,21 @@ namespace Pluralsight.Demo
     {
         static void Main(string[] args)
         {
+            var username = "ely";
+            var password = "654321";
+
+
             var userStore = new UserStore<IdentityUser>();
             var userManager = new UserManager<IdentityUser>(userStore);
 
-           var creationResult =  userManager.Create(new IdentityUser("marcos"), "123456");
+           var creationResult =  userManager.Create(new IdentityUser(username), password);
 
-            Console.WriteLine("Created:{0}", creationResult.Succeeded);
+            //Console.WriteLine("Created:{0}", creationResult.Succeeded);
+            //Console.ReadLine();
+            var user = userManager.FindByName(username);
+           var claimResult =  userManager.AddClaim(user.Id,new Claim("given_name","Ely"));
+
+            Console.WriteLine("Claim:{0}", claimResult.Succeeded);
             Console.ReadLine();
         }
     }
