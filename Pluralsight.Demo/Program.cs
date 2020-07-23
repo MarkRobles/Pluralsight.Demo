4c0884fcfefa72d5cff982dd7b05a733590790d1
Marcos Robles <ecko_lob@outlook.com>
2020-07-23 11:09:15 -0500

    Agregar archivos de proyecto.

---
+++ Pluralsight.Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluralsight.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+           var creationResult =  userManager.Create(new IdentityUser("marcos"), "123456");
+
+            Console.WriteLine("Created:{0}", creationResult.Succeeded);
+            Console.ReadLine();
+        }
+    }
+}
